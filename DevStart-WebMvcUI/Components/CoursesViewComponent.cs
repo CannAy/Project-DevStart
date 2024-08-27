3756263e8b3b548b26da3db18377c7583ed79dca
Bekir ATAÇ <atacbekir45@gmail.com>
2024-08-27 23:22:36 +0300

    son olanlar

---
+++ DevStart-WebMvcUI/Components/CoursesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevStart_Entity.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevStart_WebMvcUI.Components
+{
+    public class CoursesViewComponent : ViewComponent
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesViewComponent(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var list = await _courseService.GetAllAsync();
+            return View("Index", list);
+        }
+    }
+}
